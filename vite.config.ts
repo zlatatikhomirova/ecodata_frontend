8dd842c6847d0ad1083306c573565536924afdff
Daniil325 <potekhindaniil04@gmail.com>
2025-04-04 23:56:28 +0700

    configure imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      {
+        find: "@components",
+        replacement: fileURLToPath(
+          new URL("./src/components/", import.meta.url)
+        ),
+      },
+      {
+        find: "@pages",
+        replacement: fileURLToPath(new URL("./src/pages", import.meta.url)),
+      },
+      {
+        find: "@entities",
+        replacement: fileURLToPath(new URL("./src/entities", import.meta.url)),
+      },
+    ],
+  },
+});
